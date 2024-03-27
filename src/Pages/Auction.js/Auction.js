1252942446673f032c928780f91bffa8393cb19d
Zultaif Hassan <zultaifhassan110@gmail.com>
2024-03-27 11:00:00 +0500

    real state

--- src/Pages/Auction.js/Auction.js
+++ src/Pages/Auction.js/Auction.js
@@ -1,10 +1,15 @@
 import React from "react";
 import './Auction.css'
+import Vehicle from '../Home/Vehicle/Vehicle'
+import Jewelery from '../Home/Jewelery/Jewelery'
+import RealState from "../Home/RealState/RealState";
 
 const Auction = () => {
   return (
     <div className="max-width auction-page-first">
-      <h1>Auction</h1>
+      <Vehicle />
+      <Jewelery />
+      <RealState />
     </div>
   );
 };
